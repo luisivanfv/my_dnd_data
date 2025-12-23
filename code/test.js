e6aa09650d6ea35217c6173f8173f68a2ad3d56d
luisivanfv <luis.ivan.fv@gmail.com>
2025-12-23 00:33:00 -0600

    up

--- code/test.js
+++ code/test.js
@@ -6,6 +6,7 @@ const damageTypeIconSize = "20";
 const txtSize = '16px';
 const lookerTxtSize = '16px';
 const actionTitleTxtSize = '14px';
+const secondsPopupShown = 5;
 const damageTypes = ['Acid', 'Bludgeoning', 'Cold', 'Fire', 'Force', 'Lightning', 'Necrotic', 'Piercing', 'Poison', 'Psychic', 'Radiant', 'Slashing', 'Thunder'];
 // Create visible element
 var testMarker = document.createElement('div');
