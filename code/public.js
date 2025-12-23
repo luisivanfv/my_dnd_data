5213c721216e4ec5eb272dcd47122b32aa45fa8d
luisivanfv <luis.ivan.fv@gmail.com>
2025-12-22 21:38:36 -0600

    up

--- code/public.js
+++ code/public.js
@@ -1,5 +1,4 @@
 const websiteRoot = 'https://blindingdarkness.obsidianportal.com';const mapKeys = ['colorReplacements', 'keywordReplacements'];
-//const githubRoot = 'https://raw.githubusercontent.com/luisivanfv/my_dnd_data/main/';
 const txtSize = '16px';
 const lookerTxtSize = '16px';
 const actionTitleTxtSize = '14px';
