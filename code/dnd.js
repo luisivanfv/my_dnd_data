f654334c112509f4517847dbd5ac1bccd9d390cb
luisivanfv <luis.ivan.fv@gmail.com>
2025-12-31 14:43:21 -0600

    up

--- code/dnd.js
+++ code/dnd.js
@@ -1509,7 +1509,7 @@ function initializeTableData() {
         const encounterName = getUrlParameter('name');
         console.log('Encounter name from URL:', encounterName);
         if(encounterName) {
-            const encounterData = JSON.parse(localStorage.getItem(`encounters_${encounterName}`));
+            const encounterData = JSON.parse(localStorage.getItem(`encounters_${encounterName}.json`));
             console.log('Encounter data from localStorage:', encounterData);
             /*if (encounterData && Array.isArray(encounterData.monsters)) {
                 encounterData.monsters.forEach((monsterEntry) => {});
