0228c9908ef62d02b38c65a2f1606d085507d56d
luisivanfv <luis.ivan.fv@gmail.com>
2026-01-20 13:52:44 -0600

    up

--- code/dnd.js
+++ code/dnd.js
@@ -1548,8 +1548,8 @@ async function createCharacterSheet(characterData) {
                                 data-item-quantity="${item.quantity}"
                                 data-equipped="${item.equipped}"
                                 data-item-data='${JSON.stringify(item)}'
-                                style="background: ${item.equipped ? character.darkColor : character.secondaryColor};">
-                                <div class="item-icon"><img width="20" height="20" src="${item.iconUrl}" alt="${item.iconAlt}"/></div>
+                                style="background: ${item.equipped ? character.secondaryColor : character.darkColor};">
+                                <div class="item-icon"><img width="20" height="20" src="${item.iconUrl.replace('customSize', '20').replace('customColor', character.textColor)}" alt="${item.iconAlt}"/></div>
                                 <div class="item-name" style="color: ${character.textColor};">${item.name || `Item ${index + 1}`}</div>
                                 ${item.quantity ? `<div class="item-quantity" style="background: ${character.color}; color: ${character.secondaryTextColor};">x${item.quantity}</div>` : ''}
                                 ${item.weight ? `<div class="item-weight" style="background: ${character.color}; color: ${character.textColor};">${item.weight} kg</div>` : ''}
