57983716c375e4a5d678e7975aec26864fdc57b1
luisivanfv <luis.ivan.fv@gmail.com>
2026-01-20 16:00:20 -0600

    up

--- code/dnd.js
+++ code/dnd.js
@@ -1548,11 +1548,11 @@ async function createCharacterSheet(characterData) {
                                 data-item-quantity="${item.quantity}"
                                 data-equipped="${item.equipped}"
                                 data-item-data='${JSON.stringify(item)}'
-                                style="background: ${item.equipped ? character.secondaryColor : character.darkColor};">
-                                <div class="item-icon"><img width="20" height="20" src="${item.iconUrl.replace('customSize', '20').replace('customColor', character.textColor.replace('#', ''))}" alt="${item.iconAlt}"/></div>
+                                style="background: ${item.equipped ? character.secondaryColor : character.darkColor}; margin-bottom: 5px;">
+                                <div class="item-icon" style="margin-right: 10px;><img width="20" height="20" src="${item.iconUrl.replace('customSize', '20').replace('customColor', character.textColor.replace('#', ''))}" alt="${item.iconAlt}"/></div>
                                 <div class="item-name" style="color: ${character.textColor};">${item.name || `Item ${index + 1}`}</div>
-                                ${item.quantity ? `<div class="item-quantity" style="background: ${character.color}; color: ${character.secondaryTextColor};">x${item.quantity}</div>` : ''}
-                                ${item.weight ? `<div class="item-weight" style="background: ${character.color}; color: ${character.textColor};">${item.weight} kg</div>` : ''}
+                                ${item.quantity && item.quantity > 1 ? `<div class="item-quantity" style="background: ${character.color}; color: ${character.secondaryTextColor};">x${item.quantity}</div>` : ''}
+                                ${item.weight && item.quantity ? `<div class="item-weight" style="background: ${character.color}; color: ${character.textColor};">${item.weight * item.quantity} kg</div>` : ''}
                             </div>
                         `).join('')}
                         </div>
