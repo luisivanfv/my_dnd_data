40b4269c5ab101f203e946e36c308b7e639d3de1
luisivanfv <luis.ivan.fv@gmail.com>
2026-01-19 21:20:31 -0600

    up

--- code/obsidian.js
+++ code/obsidian.js
@@ -409,7 +409,7 @@ class SmartPoller {
     async fetchUpdates() {
         const url = new URL(window.location.href);
         const value = url.searchParams.get('name');
-        if(url.includes('charactersheet') && value) {
+        if(window.location.href.includes('charactersheet') && value) {
             console.log("We're on a character sheet, we might want to update something!");
             await updateCharacterSheet();
             //await queryDatabase();
